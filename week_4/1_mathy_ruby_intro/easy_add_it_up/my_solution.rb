9605b429b5468b6c821b32998888435eff51d7cf
George Zeng <george.x.zeng@gmail.com>
2014-06-04 10:12:30 -0400

    release 1 - pseudocode

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,19 +4,32 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
 # What is the input?
+The input is an array of numbers for the total method
+The input is an array of strings for the sentence_maker method
+
 # What is the output? (i.e. What should the code return?)
+The output is one number - their total or the sum for the total method
+The output is one string - a oncatenated sentence for the sentence_maker method
+
 # What are the steps needed to solve the problem?
+
+For the total method - 
+First we need fo create a variable equal to zero (a my_sum variable or something of that sort)
+Then we neeed to add each number in the array to the variable
+Then we need to output the final number as the sum
+
+For the sentence_maker method - 
+First we need to have an empty string - create a variable called my_sentence or something of that sort
+Then we need to add each string in the array to the varialbe, one at a time
+Then we need to output the final string as a sentence
 
 
 # 2. Initial Solution
-
-
-
 
 # 3. Refactored Solution
 
