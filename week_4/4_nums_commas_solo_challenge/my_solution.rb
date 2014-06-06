2d47dca2efb8f9b98b15e0080cbcc8d53e6feec2
George Zeng <george.x.zeng@gmail.com>
2014-06-06 19:41:03 -0400

    modified solo challenge

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -8,12 +8,30 @@
 # 1. Pseudocode
 
 # What is the input?
+
+Input is a random number
+
 # What is the output? (i.e. What should the code return?)
+
+Output is a string with commas separating every three integer places
+
+If random number < 1,000 then returns random number
+If random number >=1,000 then returns string inserting a comma every three places
+
+
 # What are the steps needed to solve the problem?
+
+1. If random number < 1,000 then return random number
+2. If random number >=1,000 then returns string
+3. Insert a comma after last 0*4+3 places of the string
+4. Insert a comma after the last 1*4+3 plages of the string if it exists
+	It not return string as is
+5. Insert a comma after the last 2*4+3 places of the string
+	If not return string as is
+6. Loop this until loop breaks
 
 
 # 2. Initial Solution
-
 
 
 
