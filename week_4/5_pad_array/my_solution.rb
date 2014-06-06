91066e6318b6bfbf81160a1fb5d63d684dfe1eb4
George Zeng <george.x.zeng@gmail.com>
2014-06-06 14:07:31 -0400

    pad solution beta

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -4,18 +4,43 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Clayton Jordan.
 
 # 1. Pseudocode
 
 # What is the input?
+
+#Array of non-negative integers
+
 # What is the output? (i.e. What should the code return?)
+
+# A new array that contains a minimum number of values and supplements the array with additional padded
+# values if the array length is less than the specified value
+
 # What are the steps needed to solve the problem?
 
+# 1. Take the pad number value
+# 2. Compare that with the length of the array
+# 3. If array.length - pad.number = positive number, then add the pad value to the end of the array in 
+# an iterative loop equal to the positive number difference
+# 4. Return the new array
+# 5. If array.length - pad.number =< 0, then just return the original array
 
 # 2. Initial Solution
 
-
+class Array
+  def pad(pad_num, pad_value)
+    pad_decider = pad_num - self.length
+    if pad_decider > 0 then 
+      pad_decider.times do pad_value
+        self.push(pad_value)
+      end
+    else 
+      self
+    end
+    self
+  end
+end
 
 
 # 3. Refactored Solution
