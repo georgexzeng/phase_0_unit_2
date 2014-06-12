a2081593315cacdddf1fd2ed7a5d237ea3a881cf
George Zeng <george.x.zeng@gmail.com>
2014-06-11 22:28:24 -0400

    interim solutions for challenge 5 and 6

--- week_5/5_virus_predictor/original_code.rb
+++ week_5/5_virus_predictor/original_code.rb
@@ -18,45 +18,32 @@ class VirusPredictor
   end
 
   def virus_effects  #HINT: What is the SCOPE of instance variables?
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    disease_report(@population_density, @population, @state) #come back to this
   end
 
   private  #what is this?  what happens if it were cut and pasted above the virus_effects method
 
-  def predicted_deaths(population_density, population, state)
+  def disease_report(population_density, population, state)
+    speed = 0.0
+  
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else 
-      number_of_deaths = (@population * 0.05).floor
-    end
-
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
-
-  end
-
-  def speed_of_spread(population_density, state) #in months
-    speed = 0.0
-
-    if @population_density >= 200
       speed += 0.5
     elsif @population_density >= 150
-      speed += 1
+      number_of_deaths = (@population * 0.3).floor
+      speed += 1.0
     elsif @population_density >= 100
+      number_of_deaths = (@population * 0.2).floor
       speed += 1.5
     elsif @population_density >= 50
-      speed += 2
+      number_of_deaths = (@population * 0.1).floor
+      speed += 2.0
     else 
+      number_of_deaths = (@population * 0.05).floor
       speed += 2.5
     end
 
-    puts " and will spread across the state in #{speed} months.\n\n"
+    print "#{@state} will lose #{number_of_deaths} people in this outbreak and will spread across the state in #{speed} months.\n\n"
 
   end
 
@@ -67,15 +54,19 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+STATE_DATA.each_key do |my_state|
+  my_report = VirusPredictor.new(my_state, STATE_DATA[my_state][:population_density], STATE_DATA[my_state][:population], STATE_DATA[my_state][:region], STATE_DATA[my_state][:regional_spread]) 
+  my_report.virus_effects
+end
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population], STATE_DATA["Alabama"][:region], STATE_DATA["Alabama"][:regional_spread]) 
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population], STATE_DATA["Alabama"][:region], STATE_DATA["Alabama"][:regional_spread]) 
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population], STATE_DATA["New Jersey"][:region], STATE_DATA["New Jersey"][:regional_spread]) 
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population], STATE_DATA["New Jersey"][:region], STATE_DATA["New Jersey"][:regional_spread]) 
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population], STATE_DATA["California"][:region], STATE_DATA["California"][:regional_spread]) 
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population], STATE_DATA["California"][:region], STATE_DATA["California"][:regional_spread]) 
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population], STATE_DATA["Alaska"][:region], STATE_DATA["Alaska"][:regional_spread]) 
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population], STATE_DATA["Alaska"][:region], STATE_DATA["Alaska"][:regional_spread]) 
+# alaska.virus_effects
--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -1,23 +1,68 @@
 # U2.W5: Class Warfare, Validate a Credit Card Number
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Matt Chan.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: 14 integers
+# Output: boolean true or false
 # Steps:
+
+<<-Steps
+1. convert the integer into an array of numbers
+2. mod 2 the index of the array and for each mod 2 index, destructively double the value
+3. mod 2 the index of the array and for each mod 2 index, destrucitvely add the digits of the value together if they're >=10
+4. add all the values of the array together 
+5. mod 10
+
+Steps
 
 
 # 3. Initial Solution
 
-# Don't forget to check on intialization for a card length
-# of exactly 16 digits
 class CreditCard
+	def initialize(num)
+		@num = num
+		unless num.to_s.length == 14 then
+			raise ArgumentError.new('Please insert 14 integers')
+		end
+	end
 
+	def double_method
+		array_num = @num.to_s.split(//).map{|chr| chr.to_i}
+		@new_array = Array.new
+		index = 0
+		array_num.each do |num|
+			if (array_num.length-index) % 2 == 0
+				num *= 2
+			end
+			@new_array << num
+			index +=1
+		end
+		return @new_array
+	end
+
+	def sum_method
+		@sum = @new_array.join.split(//).map{|chr| chr.to_i}.inject(:+)
+	end
+
+	def check_card
+		if @sum%10==0
+			true
+		else
+			false
+		end
+	end
 end
 
+card = CreditCard.new(12345123451234)
+p card.double_method
+p card.sum_method
+p card.check_card
+
+# Don't forget to check on intialization for a card length
+# of exactly 16 digits
 
 
 # 4. Refactored Solution
@@ -29,9 +74,9 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
-
-
-
+# false_card=CreditCard.new(12345123451235)
+# false_card.check_card==false
+# true_card = CreditCard.new(4408041234567893)
+# true_card.check_card==true
 
 # 5. Reflection 
