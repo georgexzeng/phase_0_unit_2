7f599e7c17a5d72a4d1a83c65cc391a83d540b8a
George Zeng <george.x.zeng@gmail.com>
2014-06-11 22:27:11 -0400

    die solutions

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,45 +1,118 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
 # Input:
+
+# Input number of sides
+
+
 # Output:
+
+# Output stating the number of sides
+# Value of the roll
+
+
 # Steps:
+
+# Define an initailize statement to initalize with a number of unique_rolls
+# Define a sides method to specify number of sides
+# Define a roll method to specify what number is rolled
 
 
 # 3. Initial Solution
 
-class Die
-  def initialize(sides)
-    # code goes here
-  end
-  
-  def sides
-    # code goes here
-  end
-  
-  def roll
-    # code goes here
-  end
-end
+# class Die
 
+#   def initialize(number)
+#   	if number<=0 
+#   		raise ArgumentError.new
+#   	else
+#   		@number=number
+#   	end
+#   end
+
+#   def sides
+#   	@number
+#   end
+  
+#   def roll
+#     @roll = rand(@number)+1
+#   end
+
+# end
+
+# puts die = Die.new(6)
+# puts die.sides # returns 6
+# puts die.roll # returns a random number between 1 and 6
 
 
 # 4. Refactored Solution
 
 
+class Die
 
+  def initialize(number)
+  	if number<=0 
+  		raise ArgumentError.new
+  	else
+  		@number=number
+  	end
+  end
 
+  def sides
+  	@number
+  end
+  
+  def roll
+    @roll = rand(@number)+1
+  end
 
+end
+
+puts die = Die.new(6)
+puts die.sides # returns 6
+puts die.roll # returns a random number between 1 and 6
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+# 1. #initalize has a single argument
+# expect(Die.instance_method(:initialize).arity).to eq 1
+    
+# 2. argument error can't be less than 1
 
+# expect {
+#         Die.new(0)
+#       }.to raise_error(ArgumentError)
 
+# expect {
+#         Die.new(-1)
+#       }.to raise_error(ArgumentError)
 
+# 3. expects no arguments to be zero
 
+# expect(Die.instance_method(:sides).arity).to be_zero
+
+# 4. returns the number of sides given on initialization
+
+# sides = rand(800)
+# die = Die.new(sides)
+
+# 5. returns a random number between 1 and the number of sides
+
+# sides = 100 + rand(100)
+# die = Die.new(sides)
+
+# possible_values = (1..sides).to_a
+# unique_rolls = Array.new(20000) { die.roll }.uniq.sort
+
+# expect(unique_rolls).to eq possible_values
 
 # 5. Reflection 
+
+# This was a rather simple example after reviewing the well-grounded Rubyist section on classes
+
+
--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -1,42 +1,122 @@
 # U2.W5: Die Class 2: Arbitrary Symbols
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
 # Input:
+# an array of strings
+
 # Output:
+# sides - number of sides by counting the array of strings
+# roll - returns one of the strings in the array
+
 # Steps:
+# 1. initalize a new die with an array called labels, which contains an array of strings
+# 2. sides = lables.length
+# 3. roll = one random string chosen from the array
 
 
 # 3. Initial Solution
 
-class Die
-  def initialize(labels)
-  end
+# class Die
+#   def initialize(labels)
+#   	if labels.length>0
+#   		@labels=labels
+#   	else 
+#   		raise ArgumentError.new
+#   	end
+#   end
 
-  def sides
-  end
+#   def sides
+#   	@sides = @labels.length
+#   end
 
-  def roll
-  end
-end
+#   def roll
+#   	@roll = @labels.sample
+#   end
+# end
 
+# die = Die.new(['A', 'B', 'C', 'D', 'E', 'F'])
+# puts die
+# puts die.sides # still returns the number of sides, in this case 6
+# puts die.roll # returns one of ['A', 'B', 'C', 'D', 'E', 'F'], randomly
 
 
 # 4. Refactored Solution
 
 
+class Die
+  def initialize(labels)
+  	if labels.length>0
+  		@labels=labels
+  	else 
+  		raise ArgumentError.new
+  	end
+  end
 
+  def sides
+  	@sides = @labels.length
+  end
 
+  def roll
+  	@roll = @labels.sample
+  end
+end
+
+die = Die.new(['A', 'B', 'C', 'D', 'E', 'F'])
+puts die
+puts die.sides # still returns the number of sides, in this case 6
+puts die.roll # returns one of ['A', 'B', 'C', 'D', 'E', 'F'], randomly
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+# 1. for #initialize - expect a single argument
 
+# expect(Die.instance_method(:initialize).arity).to eq 1
 
+# 2. for #initalize - raises an ArgumentError if list of labels is empty
 
+# expect {
+#         Die.new([])
+#       }.to raise_error(ArgumentError)
 
+# 3. for #initialize - sides can't take a zero argument
+
+# expect(Die.instance_method(:sides).arity).to be_zero
+
+# 4. for #sides - returns the number of sides given on initalization
+
+# length = 1 + rand(100)
+# sides = Array.new(length) { 'A' }
+# die = Die.new(sides)
+
+# expect(die.sides).to eq length
+
+# 5. for #roll - expects no zero arguments
+
+# expect(Die.instance_method(:roll).arity).to be_zero
+
+# 6. for #roll - returns a single letter if one label is passed in
+
+# random_letter = ('A'..'Z').to_a[rand(26)]
+# die = Die.new([random_letter])
+
+# expect(Array.new(100) { die.roll }).to eq Array.new(100) { random_letter }
+
+# 7. for #roll - returns every possible letter for a sufficiently large number of rolls 
+
+# possible_values = ('A'..'Z').to_a.shuffle.first(15)
+
+# die = Die.new(possible_values)
+
+# output = Array.new(10000) { die.roll }.uniq
+
+# expect(output.sort).to eq possible_values.sort
 
 # 5. Reflection 
+
+# Pretty easy to do as well, given a good understanding of classes.  Still not 100% sure of the necessity of 
+# specifying equality to @label with label; but actively researching that...
