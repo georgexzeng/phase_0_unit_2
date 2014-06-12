adf246f2cb7e8780793169e3ae3806df97b7d852
George Zeng <george.x.zeng@gmail.com>
2014-06-12 12:48:25 -0400

    week 5 challenge 9

--- week_5/9_gps2.1/bakery_challenge.rb
+++ week_5/9_gps2.1/bakery_challenge.rb
@@ -1,51 +1,44 @@
 # U2.W5: The Bakery Challenge (GPS 2.1)
 
 # Your Names
-# 1)
-# 2)
+# 1) George Zeng
+# 2) Joseph White
 
  # This is the file you should end up editing. 
  
-def bakery_num(num_of_people, fav_food)
-  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
-  pie_qty = 0
-  cake_qty = 0
-  cookie_qty = 0
-  
-  has_fave = false
+# method takes 2 parameters: # of people and favorite food
+# checks to ensure favorite food matches available option
+# checks to see if favorite food can serve all people 1 serving without leftovers
+# if it cannot, assigns appropriate numbers of pie, cake, or cookies (in that order)
+# to ensure 1 serving per person is prepared
 
-  my_list.each_key do |k|
-  if k == fav_food
-  has_fave = true
-  fav_food = k
+def bakery_num(num_of_people, fav_food) #all code is within this method
+
+  servings = {"pie" => 8, "cake" => 6, "cookie" => 1} # hash listing food items and servings per item
+  pies, cakes, cookies = 0, 0, 0
+
+  unless servings.has_key?(fav_food)
+    raise ArgumentError.new("You can't make that food") #raises error if fav_food didn't match pie, cake or cookie
   end
-  end
-  if has_fave == false
-  raise ArgumentError.new("You can't make that food")
-  else
-  fav_food_qty = my_list.values_at(fav_food)[0]
-  if num_of_people % fav_food_qty == 0
-    num_of_food = num_of_people / fav_food_qty
-    return "You need to make #{num_of_food} #{fav_food}(s)."
-  else num_of_people % fav_food_qty != 0
-    while num_of_people > 0
-    if num_of_people / my_list["pie"] > 0
-    pie_qty = num_of_people / my_list["pie"]
-    num_of_people = num_of_people % my_list["pie"]
-    elsif num_of_people / my_list["cake"] > 0
-    cake_qty = num_of_people / my_list["cake"]
-    num_of_people = num_of_people % my_list["cake"]
-    else
-    cookie_qty = num_of_people
-    num_of_people = 0
-    end
-    end
-    return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
-    end
-  end
+
+  if num_of_people % servings[fav_food] == 0
+    fav_food_qty = num_of_people / servings[fav_food]
+    return "You need to make #{fav_food_qty} #{fav_food}(s)."
+
+# assigns numbers of pie, cake, and/or cookies to be prepared so that 1 serving
+# is available for each person -- pie is assigned first, then cake, then cookies
+
+  else 
+    pies = num_of_people / servings["pie"]
+    num_of_people = num_of_people % servings["pie"]
+    cakes = num_of_people / servings["cake"]
+    num_of_people = num_of_people % servings["cake"]
+    cookies = num_of_people
+end
+
+  return "You need to make #{pies} pie(s), #{cakes} cake(s), and #{cookies} cookie(s)."
 end
  
-
 #-----------------------------------------------------------------------------------------------------
 #DRIVER TEST CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
 # These are the tests to ensure it's working. 
@@ -59,5 +52,3 @@ p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie
 
 # You SHOULD change this driver test code. Why? Because it doesn't make sense.
 p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
- 
- 
--- week_5/9_gps2.1/my_solution.rb
+++ week_5/9_gps2.1/my_solution.rb
@@ -2,20 +2,36 @@
 
 # I worked on this challenge with: 
 
-
-
-
-
+Joseph White
 
 # Our Refactored Solution
 
+def bakery_num(num_of_people, fav_food) #all code is within this method
 
+  servings = {"pie" => 8, "cake" => 6, "cookie" => 1} # hash listing food items and servings per item
+  pies, cakes, cookies = 0, 0, 0
 
+  unless servings.has_key?(fav_food)
+    raise ArgumentError.new("You can't make that food") #raises error if fav_food didn't match pie, cake or cookie
+  end
 
+  if num_of_people % servings[fav_food] == 0
+    fav_food_qty = num_of_people / servings[fav_food]
+    return "You need to make #{fav_food_qty} #{fav_food}(s)."
 
+# assigns numbers of pie, cake, and/or cookies to be prepared so that 1 serving
+# is available for each person -- pie is assigned first, then cake, then cookies
 
+  else 
+    pies = num_of_people / servings["pie"]
+    num_of_people = num_of_people % servings["pie"]
+    cakes = num_of_people / servings["cake"]
+    num_of_people = num_of_people % servings["cake"]
+    cookies = num_of_people
+end
 
-
+  return "You need to make #{pies} pie(s), #{cakes} cake(s), and #{cookies} cookie(s)."
+end
 
 #DRIVER TEST CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
 # These are the tests to ensure it's working. 
@@ -29,8 +45,6 @@ p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie
 
 # You SHOULD change this driver test code. Why? Because it doesn't make sense.
 p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
-
-
 
 
 #  Reflection 
