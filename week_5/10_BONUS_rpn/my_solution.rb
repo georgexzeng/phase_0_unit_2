873608604f299ff0e0af64c9c3491c1c44018501
George Zeng <george.x.zeng@gmail.com>
2014-06-16 01:35:07 -0400

    bonus challenge

--- week_5/10_BONUS_rpn/my_solution.rb
+++ week_5/10_BONUS_rpn/my_solution.rb
@@ -5,31 +5,80 @@
 
 # 2. Pseudocode
 
-# Input:
+# Input: 
+# - evaluate method takes a string of numbers and operators
+# 
 # Output:
+# - evaluate will output the result of the calculation
+#
 # Steps:
-
+# - split string into array
+# - count from left
+# - find first evaluator (+,-, *, /)
+# - take two numbers to the left of evaluator
+# - operate with evaluator
+# - replace two numbers and operator with result
 
 # 3. Initial Solution
 
-class RPNCalculator
-  # Define your methods, here!
-end
-
-
+# class RPNCalculator
+#   def evaluate(input_numbers)
+#     my_array = input_numbers.split(' ')
+#     results = []
+#     while my_array.length > 1
+#       my_array.each do |item|
+#         p item + "\n\n"
+#         if item.match(/(\*|\+|\-(?!\d))/)
+#           ahead1 = my_array[my_array.index(item) - 1]
+#           ahead2 = my_array[my_array.index(item) - 2]
+#           my_array[my_array.index(ahead2)] = ahead2.to_i.public_send(item, ahead1.to_i).to_s
+#           my_array.delete_at(my_array.index(item))
+#           my_array.delete_at(my_array.index(ahead1))
+#         end
+#       end
+#     end
+#     my_array[0].to_i
+#   end
+# end
 
 # 4. Refactored Solution
 
-
-
-
+class RPNCalculator
+  def evaluate(input_numbers)
+    my_array = input_numbers.split(' ')
+    while my_array.length > 1
+      my_array.each do |item|
+        if item.match(/(\*|\+|\-(?!\d))/)
+          ahead1 = my_array[my_array.index(item) - 1]
+          ahead2 = my_array[my_array.index(item) - 2]
+          my_array[my_array.index(ahead2)] = ahead2.to_i.public_send(item, ahead1.to_i).to_s
+          my_array.delete_at(my_array.index(item))
+          my_array.delete_at(my_array.index(ahead1))
+        end
+      end
+    end
+    my_array[0].to_i
+  end
+end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+puts RPNCalculator.instance_method(:evaluate).arity == 1
 
+rpn = RPNCalculator.new()
+p rpn.evaluate('0') == 0
+p rpn.evaluate('-1') == -1
+p rpn.evaluate('1 1 +') == 2
+p rpn.evaluate('1 -1 +') == 0
+p rpn.evaluate('2 2 *') == 4
+p rpn.evaluate('5 10 -') == -5
+p rpn.evaluate('1 2 3 4 + + +') == 1 + 2 + 3 + 4
+p rpn.evaluate('1 2 + 3 4 + *') == (2 + 1) * (3 + 4)
+p rpn.evaluate('20 10 5 4 + * -') ==  20 - ((5 + 4) * 10)
 
-
+a, b, c = Array.new(3) {rand(100)}
+p rpn.evaluate("#{a} #{b} - #{c} *") == (a-b)*c
 
 
 # 5. Reflection 
